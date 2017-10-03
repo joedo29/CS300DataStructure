4c77280e54ad7bab4e01ac68cc3d23e411145a4c
Joe Do <joedo@Joes-MacBook-Pro.local>
2017-10-03 11:29:12 -0700

    my third commit

--- Assignment01/Assignment01.cpp
+++ Assignment01/Assignment01.cpp
@@ -1,10 +1,33 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+
 using namespace std;
 
 int main (){
-	int x = 10;
-	int *p = &x;
-	int &r = *p;
-	cout << r << endl;
-	cout << "Hello World!";
+	ifstream file;
+	file.open("phonebook.txt");
+	cout << "MY PHONEBOOK APPLICATION" << endl;
+	cout << "Please choose an operation:" << endl;
+	cout << "A (Add) | S (Search) | D (Delete) | L (List) | Q (Quit): " << endl;
+	cout << "enter name: ";
+	string input;
+	cin >> input;
+	file >> input;
+	cout << endl;
+	cout << "enter phone number: ";
+	int inputNumber;
+	cin >> inputNumber;
+	file >> inputNumber;
+
+
+
+//	ofstream outfile("output.txt");
+//	outfile << 2*a << " " << 2*b;
+
+
+
+	return 0;
+
 }
