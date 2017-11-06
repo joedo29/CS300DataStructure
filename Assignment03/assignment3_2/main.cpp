9d1c86502a3175d3b7a93337bb462ac257a8dbdf
Joe Do <joedo@Joes-MacBook-Pro.local>
2017-11-06 13:10:27 -0800

    Initial commit for Assignment 3. Set up new project.

---
+++ Assignment03/assignment3_2/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  cout << "Hello 2222" << endl;
+  // cout << "So, what do you wanna say to JOE? ";
+  // string s;
+  // cout << "Type your message: ";
+  // getline(cin, s);
+  // cout << "I will tell JOE that you wanna say ' " << s << " ' " <<'\n';
+
+  return 0;
+}
