d5b8b713bd5799ba81eb307cfd79cba5f0046927
Joe Do <joedo@Joes-MacBook-Pro.local>
2017-11-11 11:32:19 -0800

    Finished recursive method to revert an integer

---
+++ Assignment03/assignment3_4/main.cpp
@@ -0,0 +1,50 @@
+//============================================================================
+// Name        : main.cpp
+// Author      : Joe Do
+// Version     : 1.0
+// Copyright   : Your copyright notice
+// Description : Write a C++ program which reads a positive number from the user and
+// displays the decimal digits of an integer in reverse order.
+//============================================================================
+
+#include <iostream>
+#include "Stack.h"
+#include <assert.h>
+using namespace std;
+
+// recursive method to convert a number
+void reverseDigits(int i){
+  if(i == 0) // base case
+    return 0;
+
+  if(i == 1) // base case
+    return 1;
+
+  if(i % 10 == i)
+    return;
+
+  else{
+    cout << i % 10; // 256 % 10 = 6
+    reverse(i / 10); // 256 / 10 = 25
+  }
+} // closes reverseDigits method
+
+// Non-recursive method
+void reverseDigits(string s){
+  Stack<char> numbers;
+
+  for (char & c : s)
+    numbers.push(c);
+
+  while(!numbers.isEmpty())
+    cout<<numbers.pop();
+}
+
+int main() {
+  int i;
+  cout << "Enter a number: ";
+  cin >> i;
+
+  reverseDigits(i);
+	return 0;
+}
